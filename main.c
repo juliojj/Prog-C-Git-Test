13c313e39534205e94a86755ff113bed06f5b409
Julio <julio.silveira@ub.edu>
2026-09-03 12:49:24 +0200

    generating a conflict

--- main.c
+++ main.c
@@ -11,6 +11,6 @@ int main () {
     assert ( suma(2 , 3) == 5) ;
     assert ( suma( -1 , 1) == 0) ;
     assert ( suma(10 , 10) == 20) ;
-    printf ( "EXIT : Tots els tests han passat correctament !\ n" ) ;
+    printf ( "changing from local EXIT : Tots els tests han passat correctament !\ n" ) ;
     return 0;
 }
