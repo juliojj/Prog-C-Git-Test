1d0f003cb84f069efc5ba1ee31b0c75eb228d4d2
Julio <julio.silveira@ub.edu>
2026-09-03 11:50:59 +0200

    testing assert

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+# include < stdio .h >
+# include < assert .h > // Llibreria per fer tests
+// Funci ó que volem testejar
 
-int main(){
-    printf("Hello world!\n")
+int suma ( int a , int b ) {
+    return a + b ;
+}
+
+int main () {
+    // Tests : Si la condici ó s falsa , el programa s ’ atura amb error
+    assert ( suma (2 , 3) == 5) ;
+    assert ( suma ( -1 , 1) == 0) ;
+    assert ( suma (10 , 10) == 20) ;
+    printf ( " EXIT : Tots els tests han passat correctament !\ n " ) ;
     return 0;
 }
